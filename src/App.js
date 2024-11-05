41a65a35b769d0613258e1e697758ce000aeb523
Toby Emeboh <Toby.Emeboh@student.rocva.nl>
2024-11-05 03:03:01 +0100

    REST API Deel 2

--- src/App.js
+++ src/App.js
@@ -1,62 +1,22 @@
 import React from 'react';
-const questions = [
-  {
-      questionText: 'Wat is de hoofdstad van Frankrijk?',
-      answerOptions: [
-          { answerText: 'New York', isCorrect: false },
-          { answerText: 'London', isCorrect: false },
-          { answerText: 'Paris', isCorrect: true },
-          { answerText: 'Dublin', isCorrect: false },
-      ],
-  },
-  {
-      questionText: 'Wie is de oprichter van Tesla?',
-      answerOptions: [
-          { answerText: 'Jeff Bezos', isCorrect: false },
-          { answerText: 'Elon Musk', isCorrect: true },
-          { answerText: 'Bill Gates', isCorrect: false },
-          { answerText: 'Tony Stark', isCorrect: false },
-      ],
-  },
-  {
-      questionText: 'Welk bedrijf was eind jaren 90 bijna failliet?',
-      answerOptions: [
-          { answerText: 'Apple', isCorrect: true },
-          { answerText: 'Intel', isCorrect: false },
-          { answerText: 'Amazon', isCorrect: false },
-          { answerText: 'Microsoft', isCorrect: false },
-      ],
-  },
-  {
-      questionText: 'Hoeveel Harry Potter boeken zijn er?',
-      answerOptions: [
-          { answerText: '1', isCorrect: false },
-          { answerText: '4', isCorrect: false },
-          { answerText: '6', isCorrect: false },
-          { answerText: '7', isCorrect: true },
-      ],
-  },
-  {
-      questionText: 'Wat is GEEN Cryptomunt?',
-      answerOptions: [
-          { answerText: 'Bitcoin', isCorrect: false },
-          { answerText: 'EOS', isCorrect: false },
-          { answerText: 'Pancake Swap', isCorrect: false },
-          { answerText: 'XOTA-XL', isCorrect: true },
-      ],
-  },
-  {
-    questionText: 'Welke van deze personen is allochtoon ',
-    answerOptions: [
-        { answerText: 'Qian', isCorrect: false },
-        { answerText: 'Kyran', isCorrect: false },
-        { answerText: 'Efe', isCorrect: true },
-        { answerText: 'Dim', isCorrect: false },
-    ],
-},
-];
+
 class App extends React.Component {
+  componentDidMount() {
+    this.getData();
+  }
   
+  getData() {
+    fetch('http://localhost:8080/quiz/api-get')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        jsonLoaded: true,
+        jsonData: data
+      });
+    })
+    .catch(console.log);
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -72,7 +32,7 @@ class App extends React.Component {
       this.setState( {'score': this.state.score+1} );
     }else{
     }
-    if ( this.state.vraagNr < questions.length-1) {
+    if ( this.state.vraagNr < this.state.jsonData.length-1) {
       this.setState( {'vraagNr': this.state.vraagNr+1} );
     } else {
       this.setState( { 'stop': true } );
@@ -81,7 +41,12 @@ class App extends React.Component {
   }
   
   render() {
+    if ( ! this.state.jsonLoaded ) {
+      return (  <div className="app">loading...</div>)
+    }
+    var questions=this.state.jsonData;
     var antwoorden=questions[this.state.vraagNr].answerOptions;
+  
     return (
       <div className="app">
         { this.state.stop  ?  
